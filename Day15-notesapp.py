4b31676da6bee94af1ae5f95d83660efa6f45fab
Ria chandnani <riachan2007@gmail.com>
2026-07-01 13:36:54 +0530

    Enhance notes app with search feature
    Added functionality to search for notes in the notes app.

--- Day15-notesapp.py
+++ Day15-notesapp.py
@@ -1,18 +1,34 @@
-
 #print("this is learning file handling")
+
 print("this is notes app")
+
 while True:
- choice=input("1. add note 2. view note 3. exit ")
- if choice=="1":
-  print("this is add note section")
-  file=open("text.txt","a")
-  note=input("Enter your note: ")
-  file.write(note + "\n")
-  
- elif choice=="2":
-   print("this is view note section")
-   file=open("text.txt","r")
-   print(file.read())
-   notes = file.readlines()
-   for note in notes:
-     print(note)
+    choice = input("1. add note 2. view note 3. search note 4. exit ")
+
+    if choice == "1":
+        print("this is add note section")
+        file = open("text.txt", "a")
+        note = input("Enter your note: ").lower()
+        file.write(note + "\n")
+        file.close()
+        
+    elif choice == "2":
+        print("this is view note section")
+        file = open("text.txt", "r")
+        print(file.read())
+
+    elif choice == "3":
+        print("this is search note section")
+        file = open("text.txt", "r")
+        search = input("Enter the note you want to search: ").lower()
+        notes = file.readlines()
+        for note in notes:
+            if search in note:
+                print("note found")
+                print(note)
+
+        
+    elif choice == "4":
+        print("exiting the app")
+        file.close()
+        break
