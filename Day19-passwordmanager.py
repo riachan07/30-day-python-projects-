794a1a90d6036ea6d79f61423b975086caf11e78
Ria chandnani <riachan2007@gmail.com>
2026-07-03 13:25:42 +0530

    Create Day19-passwordmanager.py

---
+++ Day19-passwordmanager.py
@@ -0,0 +1,21 @@
+import random
+import string 
+print("This is password generator app")
+letters=int(input("Enter the number of letters you want in your password: "))
+numbers=int(input("Enter the number of numbers you want in your password: "))
+symbols=int(input("Enter the number of symbols you want in your password: "))
+#print(string.ascii_letters)
+#print(string.digits)
+#print(string.punctuation)
+password=[]
+for i in range(letters):
+    password.append(random.choice(string.ascii_letters))
+for i in range(numbers):
+    password.append(random.choice(string.digits))
+for i in range(symbols):
+    password.append(random.choice(string.punctuation))
+print("Your password is: ", password)
+
+a=password
+random.shuffle(a)
+print("Your password is: ", ''.join(a))
